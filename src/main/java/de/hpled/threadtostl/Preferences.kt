e36162c3901ff4c41552128a9992d205038c363c
fwiecz <55588979+fwiecz@users.noreply.github.com>
2019-10-18 15:11:10 +0200

    Preferences UI

--- src/main/java/de/hpled/threadtostl/Preferences.kt
+++ src/main/java/de/hpled/threadtostl/Preferences.kt
@@ -1,16 +1,38 @@
 package de.hpled.threadtostl
 
+import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
+import javafx.scene.control.ComboBox
+import javafx.scene.control.Spinner
 import javafx.scene.layout.VBox
 import java.net.URL
 import java.util.*
 
+enum class Type {
+    NUT, BOLT
+}
+
 class Preferences : VBox(), Initializable {
+    @FXML
+    private lateinit var typeComboBox: ComboBox<String>
+    @FXML
+    private lateinit var threadDiaSpinner: Spinner<Double>
+    @FXML
+    private lateinit var threadLengthSpinner: Spinner<Double>
+    @FXML
+    private lateinit var threadAngleSpinner: Spinner<Double>
+    @FXML
+    private lateinit var threadStepSpinner: Spinner<Double>
+
+    private val resources = ResourceBundle.getBundle("strings")!!
+    private val types = mapOf<Type, String>(
+            Type.BOLT to resources.getString("label.typeBolt"),
+            Type.NUT to resources.getString("label.typeNut")
+    )
 
     init {
-        val res = ResourceBundle.getBundle("strings")
-        FXMLLoader(javaClass.getResource("/prefs.fxml"), res).apply {
+        FXMLLoader(javaClass.getResource("/prefs.fxml"), resources).apply {
             setRoot(this@Preferences)
             setController(this@Preferences)
             load()
@@ -18,6 +40,7 @@ class Preferences : VBox(), Initializable {
     }
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-
+        typeComboBox.items.setAll(types.values)
+        typeComboBox.selectionModel.select(0)
     }
 }
